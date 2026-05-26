704a4823c87c420b73e0560a273cefe9bfa580ed
TanujBansal1 <tanaybansal9030@gmail.com>
2026-05-26 10:51:17 +0530

    Solution

--- tf_code_1/providers.tf
+++ tf_code_1/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = ""
-    key    = ""
-    region = ""
+    bucket = "cmtr-sghaq6oo-backend-bucket-1779771041"
+    key    = "tf_code_1.tfstate"
+    region = "eu-west-1"
   }
 }
--- tf_code_2/providers.tf
+++ tf_code_2/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = ""
-    key    = ""
-    region = ""
+    bucket = "cmtr-sghaq6oo-backend-bucket-1779771041"
+    key    = "tf_code_2.tfstate"
+    region = "eu-west-1"
   }
 }
