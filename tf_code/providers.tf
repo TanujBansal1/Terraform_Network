5c26cc8d75f93a6b3ddffd10579723c0e07b377e
TanujBansal1 <tanaybansal9030@gmail.com>
2026-05-28 15:51:36 +0530

    Solution

--- tf_code/providers.tf
+++ tf_code/providers.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cmtr-sghaq6oo-backend-new-bucket-1779959646"
+    bucket = "cmtr-sghaq6oo-backend-bucket-1779961793"
     key    = "tf_code.tfstate"
     region = "eu-west-1"
   }
---
+++ tf_code/resources.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_policy" "custom_policy" {
+  name        = "cmtr-sghaq6oo-iam-policy"
+  description = "Custom role with limited permissions"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:*",
+          "s3:*"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
